642ccde6172342b483930671bbbabb9145b47524
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-22 17:47:48 -0300

    feat: created blogPost models and migrations and relationship between blogPost and user

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,26 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define("BlogPost", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true
+    },
+  }, {
+    timestamps: false,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'users' });
+  };
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
