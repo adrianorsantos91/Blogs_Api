1272a81510eb982beb11d322b76dbb5a908ee035
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-24 18:32:50 -0300

    feat: created the manager of routes derived from /categories

---
+++ src/routers/categorieRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const categorieController = require('../controllers/categorieController');
+const { validateName } = require('../middlewares/validatedCategories');
+const authenticateToken = require('../middlewares/auth.middleware');
+
+const router = express.Router();
+
+router.post('/', authenticateToken, validateName, categorieController.createCategory);
+
+module.exports = router;
