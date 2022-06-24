bc9a5b4519904d5ea3961f30bc410e1ed6e74418
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-24 11:29:09 -0300

    feat: Added the /user route access validations

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 // const authenticationMiddleware = require('../middlewares/auth.middleware');
+const { validateName, validateEmail, validatePassword } = require('../middlewares/validatedUser');
 
 const router = express.Router();
 
-router.post('/', userController.auth);
+router.post('/', validateName, validateEmail,
+validatePassword, userController.createUser);
 
 module.exports = router;
