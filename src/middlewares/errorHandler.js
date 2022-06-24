8c730386faa22f1623b09e9a2c2b8c3f69b082ca
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-24 11:26:59 -0300

    fix: included 'err.message' checker

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,2 +1,4 @@
-module.exports = (err, req, res, _next) =>
+module.exports = (err, req, res, _next) => {
+  console.log('ErrorHandler: %s', err.message);
   res.status(err.status || 500).json({ message: err.message || 'Erro inesperado.' });
+};
