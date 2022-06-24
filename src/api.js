dcbe4560b98361e9fad559f7c4eb786d6f7e6d50
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-23 22:43:33 -0300

    fix: included /login route and error middleware

--- src/api.js
+++ src/api.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+require('express-async-errors');
+const loginRouters = require('./routers/loginRouter');
+const errorMiddleware = require('./middlewares/errorHandler');
 // ...
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/login', loginRouters);
+
+app.use(errorMiddleware);
 
 // ...
 
