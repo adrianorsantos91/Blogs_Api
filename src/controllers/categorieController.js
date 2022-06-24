a909ac5bbe17e86a9bca578f22d972679bac7fc0
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-24 18:43:23 -0300

    feat: created function to get all categories in controllers layer

--- src/controllers/categorieController.js
+++ src/controllers/categorieController.js
@@ -5,6 +5,12 @@ const createCategory = async (req, res) => {
   return res.status(201).json(category);
 };
 
+const getCategoriesAll = async (_req, res) => {
+  const categoriesAll = await categorieService.getCategoriesAll();
+  return res.status(200).json(categoriesAll);
+};
+
 module.exports = {
   createCategory,
+  getCategoriesAll,
 };
