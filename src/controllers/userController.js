cb5d782c522fd140dfb3d360385475a902ecbc24
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-24 11:30:52 -0300

    feat: Implemented existing email condition returning 409 status

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,19 @@
-const auth = async (req, res) => {
-  console.log('teste', req.body);
-  return res.status(400).send('teste');
+const userService = require('../services/userService');
+const loginService = require('../services/loginService');
+// const authenticateToken = require('../middlewares/auth.middleware');
+
+const createUser = async (req, res) => {
+  const create = await userService.createUser(req.body);
+
+  if (!create) {
+    return res.status(409).json({ message: 'User already registered' });
+  }
+
+  const token = await loginService.authentication(req.body);
+
+  return res.status(201).json(token);
 };
 
 module.exports = {
-  auth,
+  createUser,
 };
