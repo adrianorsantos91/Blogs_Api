9e0dea7766e3827f617d8bac2b284592cef65e3d
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-27 18:29:29 -0300

    feat: included the function to get all Posts with users and categories

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,30 +1,41 @@
 const { User, BlogPost, Category, PostCategory } = require('../database/models');
 
 const createPost = async (email, { title, content, categoryIds }) => {
+  // try {
   const userId = await User.findOne({ where: { email } });
-
-  const { count } = await Category.findAndCountAll({
-    where: { id: categoryIds },
-  });
+  const { count } = await Category.findAndCountAll({ where: { id: categoryIds } });
 
   if (count === 0) return false;
-
-  const post = await BlogPost.create({
-    title,
-    content,
-    userId: userId.id,
-  });
+  // const blogPostAndCategorie = await sequelize.transaction(async (t) => {
+  const post = await BlogPost.create({ title, content, userId: userId.id });
 
   categoryIds.forEach(async (category) => {
-    await PostCategory.create({
-      postId: post.id,
-      categoryId: category,
-    });
+    await PostCategory.create({ postId: post.id, categoryId: category });
   });
 
   return post;
 };
 
+const getPostAll = async () => {
+    const posts = await BlogPost.findAll({
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: { exclude: ['password'] },
+        },
+        {
+          model: Category,
+          as: 'categories',
+          through: { attributes: [] },
+        },
+      ],
+    });
+
+    return posts;
+};
+
 module.exports = {
   createPost,
+  getPostAll,
 };
