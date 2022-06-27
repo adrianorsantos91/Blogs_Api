176ddaa77f8bc5c2809351f4286ff8fbe2689162
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-27 19:45:11 -0300

    feat: implemented function to get post by id in controller layer

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -18,7 +18,18 @@ const getPostAll = async (_req, res) => {
   return res.status(200).json(postAll);
 };
 
+const getPostById = async (req, res) => {
+  const { id } = req.params;
+  const [getPost] = await postService.getPostById(id);
+  console.log('getPost: %s', getPost);
+  if (!getPost) {
+    return res.status(404).json({ message: 'Post does not exist' });
+  }
+  return res.status(200).json(getPost);
+};
+
 module.exports = {
   createPost,
   getPostAll,
+  getPostById,
 };
