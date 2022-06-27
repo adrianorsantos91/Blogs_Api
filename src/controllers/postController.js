f371e599359db31b99b09ba1a538a44f3cb095fe
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-27 17:01:26 -0300

    feat: implemented requirement 12

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -4,7 +4,6 @@ const createPost = async (req, res) => {
   console.log('ReqBody', req.body);
   const email = res.locals.payload;
   const post = await postService.createPost(email, req.body);
-  console.log('Post: %s', post);
 
   if (!post) {
     return res.status(400).json({ message: '"categoryIds" not found' });
