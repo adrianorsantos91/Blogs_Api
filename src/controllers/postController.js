862fe769cc05d8e161e08774b84db21858430bdf
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-27 13:03:58 -0300

    feat: created the function to create new posts in the controllers layer

---
+++ src/controllers/postController.js
@@ -0,0 +1,18 @@
+const postService = require('../services/postService');
+
+const createPost = async (req, res) => {
+  console.log('ReqBody', req.body);
+  const email = res.locals.payload;
+  const post = await postService.createPost(email, req.body);
+  console.log('Post: %s', post);
+
+  if (!post) {
+    return res.status(400).json({ message: '"categoryIds" not found' });
+  }
+
+  return res.status(201).json(post);
+};
+
+module.exports = {
+  createPost,
+};
