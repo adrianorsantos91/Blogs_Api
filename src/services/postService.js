93015ce8c66caf08ac1c622bd7f20e30a0daa1a3
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-27 21:45:30 -0300

    feat: implemented the function of updating posts according to the owning user in the service layer

--- src/services/postService.js
+++ src/services/postService.js
@@ -2,12 +2,12 @@ const { User, BlogPost, Category, PostCategory } = require('../database/models')
 
 const createPost = async (email, { title, content, categoryIds }) => {
   // try {
-  const userId = await User.findOne({ where: { email } });
+  const user = await User.findOne({ where: { email } });
   const { count } = await Category.findAndCountAll({ where: { id: categoryIds } });
 
   if (count === 0) return false;
   // const blogPostAndCategorie = await sequelize.transaction(async (t) => {
-  const post = await BlogPost.create({ title, content, userId: userId.id });
+  const post = await BlogPost.create({ title, content, userId: user.id });
 
   categoryIds.forEach(async (category) => {
     await PostCategory.create({ postId: post.id, categoryId: category });
@@ -57,8 +57,29 @@ const getPostById = async (id) => {
   return posts;
 };
 
+const updatePostById = async (id, email, { title, content }) => {
+  const checkEmail = await User.findOne({ where: { email } });
+
+  const checkUser = await BlogPost.findOne({ where: { userId: id } });
+
+  if (checkEmail.id !== checkUser.userId) return false;
+
+  await BlogPost.update({ title, content }, { where: { id } });
+
+  const [updatePost] = await BlogPost.findAll({
+    where: { id },
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+
+  return updatePost;
+};
+
 module.exports = {
   createPost,
   getPostAll,
   getPostById,
+  updatePostById,
 };
