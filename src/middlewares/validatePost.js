a57a3f67718ce686f506716c668de3b7b3636438
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-27 21:43:08 -0300

    feat: created validation middleware for post update

--- src/middlewares/validatePost.js
+++ src/middlewares/validatePost.js
@@ -19,7 +19,20 @@ const validateCategoryIds = (req, res, next) => {
   next();
 };
 
+const validateUpdateContainsFields = (req, res, next) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).json(
+      { message: 'Some required fields are missing' },
+    );
+  }
+
+  next();
+};
+
 module.exports = {
   validateContainsFields,
   validateCategoryIds,
+  validateUpdateContainsFields,
 };
