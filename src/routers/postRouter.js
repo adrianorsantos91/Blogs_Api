ee5192af7562e09e74301e60646c9f8bba98b6d1
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-27 21:44:09 -0300

    update: included PUT verb in /post/:id route

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const postController = require('../controllers/postController');
 const authenticateToken = require('../middlewares/auth.middleware');
-const { validateContainsFields, validateCategoryIds } = require('../middlewares/validatePost');
+const { validateContainsFields, validateCategoryIds,
+  validateUpdateContainsFields } = require('../middlewares/validatePost');
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router.post('/', authenticateToken,
 validateContainsFields, validateCategoryIds, postController.createPost);
 router.get('/', authenticateToken, postController.getPostAll);
 router.get('/:id', authenticateToken, postController.getPostById);
+router.put('/:id', authenticateToken, validateUpdateContainsFields, postController.updatePostById);
 
 module.exports = router;
